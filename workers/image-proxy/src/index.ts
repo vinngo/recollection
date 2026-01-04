08c77e3be8fe8dbcc444ab5092998a18c24edbb4
vinngo <ngovincent9593@gmail.com>
2026-01-03 17:59:45 -0800

    add: finished image proxy worker

--- workers/image-proxy/src/index.ts
+++ workers/image-proxy/src/index.ts
@@ -11,8 +11,25 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	RECOLLECTION_BUCKET: R2Bucket;
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx): Promise<Response> {
+		const imageUrl = new URL(request.url).pathname.slice(1);
+
+		const object = await env.RECOLLECTION_BUCKET.get(imageUrl);
+
+		if (!object) {
+			return new Response('Image not found', { status: 404 });
+		}
+
+		const headers = new Headers();
+		object.writeHttpMetadata(headers);
+		headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+		headers.set('Access-Control-Allow-Origin', '*');
+
+		return new Response(object.body, { headers });
 	},
 } satisfies ExportedHandler<Env>;
